064fa30229dc1d5e6c61522112cb71714e1a08cf
Andrew Ott <andrewjott@icloud.com>
2016-08-29 16:42:11 -0700

    Initial Commit

---
+++ Birthday2/app/src/main/java/com/andrewott/android/aca/birthday/CakeSize.java
@@ -0,0 +1,22 @@
+package com.andrewott.android.aca.birthday;
+
+/**
+ * Created by andrewott on 8/29/16.
+ */
+public class CakeSize {
+    int height = 71;
+    int weight = 150;
+    int depth = 34;
+
+    void showAttributes() {
+
+        System.out.println("How tall dat cake be: " + height);
+        System.out.println("How heavy dat cake be: " + weight);
+        System.out.println("How deep it be dawg: " + depth);
+
+
+    }
+
+
+
+}
